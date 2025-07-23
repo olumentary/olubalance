3f33f8c25a12db62bc652b9a5539cb787597b22e
Kevin Custer <kevin@kevin-custer.com>
2025-07-23 17:25:09 -0400

    Update staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -102,5 +102,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = ENV.fetch('STORAGE_SERVICE', 'linode_dev').to_sym
 end
